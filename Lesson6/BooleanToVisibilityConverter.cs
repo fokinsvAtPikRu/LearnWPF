46c13c8d763ec4ad3a8b167ea01263ff8edcce37
Фокин Серей <fokinsv@pik.ru>
2025-07-27 11:14:14 +0400

    Урок 6 выполнен

---
+++ Lesson6/BooleanToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Lesson6
+{
+    internal class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool visiblity = (bool)value; 
+            return visiblity? System.Windows.Visibility.Visible:System.Windows.Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return true;
+        }
+    }
+}
