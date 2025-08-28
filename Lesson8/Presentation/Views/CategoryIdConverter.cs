a16990e16574ae0c47f323464b9730498b785ef9
Фокин Серей <fokinsv@pik.ru>
2025-08-28 07:23:35 +0400

    не грузятся картинки

---
+++ Lesson8/Presentation/Views/CategoryIdConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Lesson8.Presentation.Views
+{
+    public class CategoryIdConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)value == 0)
+                return "Presentation/Icons/Icon1.png";
+            if ((int)value == 0)
+                return "Presentation/Icons/Icon2.png";
+            return "Presentation/Icons/Icon3.png";
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }    
+    
+}
